bdafef26ff7d6fd816b31ed21b410df597d9f1c5
Feiko Gorter <gorterfeiko@hotmail.com>
2021-04-29 20:26:42 +0200

    Some changes

---
+++ StaticBlogExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+
+namespace StaticBlogExample
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args) =>
+            await Bootstrapper
+            .Factory
+            .CreateWeb(args)
+            .AddSetting(WebKeys.ExcludedPaths,
+                new List<NormalizedPath>
+                {
+                    new NormalizedPath("input/admin"),
+                })
+            .RunAsync();
+    }
+}
